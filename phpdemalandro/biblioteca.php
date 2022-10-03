55ec415d7d3ca626915f4e399a2212712b462c32
THE LEGEND <87276808+gguisales@users.noreply.github.com>
2022-10-03 11:24:16 -0300

    Update biblioteca.php

--- phpdemalandro/biblioteca.php
+++ phpdemalandro/biblioteca.php
@@ -1,5 +1,3 @@
-
-
 <?php
   $user = 'root';
   $pass = '';
@@ -106,17 +104,3 @@
     }
   }
 ?>
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
